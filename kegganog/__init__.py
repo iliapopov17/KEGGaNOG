096cd9e0a036ad8d980d3ce17349f95c70b5c6e1
Ilia Popov <iljapopov17@gmail.com>
2025-04-15 13:19:13 +0300

    add init to run APIs

--- kegganog/__init__.py
+++ kegganog/__init__.py
@@ -1,4 +1,7 @@
-from .boxplot import boxplot
-from .corrnet import correlation_network
+from .kgnplot.boxplot import boxplot
+from .kgnplot.corrnet import correlation_network
+from .kgnplot.barplot import barplot
+from .kgnplot.radarplot import radarplot
+from .kgnplot.heatmap import heatmap
 
-__all__ = ["boxplot", "correlation_network"]
+__all__ = ["boxplot", "correlation_network", "barplot", "radarplot", "heatmap"]
