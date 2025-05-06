4ab693cfdc3b3224a84d0ba613bccc4100d745ef
Ilia Popov <iljapopov17@gmail.com>
2025-05-06 13:03:56 +0300

    add streamgraph and stacked_barplot API access

--- kegganog/__init__.py
+++ kegganog/__init__.py
@@ -3,5 +3,15 @@ from .kgnplot.corrnet import correlation_network
 from .kgnplot.barplot import barplot
 from .kgnplot.radarplot import radarplot
 from .kgnplot.heatmap import heatmap
+from .kgnplot.streamgraph import streamgraph
+from .kgnplot.stackedbar import stacked_barplot
 
-__all__ = ["boxplot", "correlation_network", "barplot", "radarplot", "heatmap"]
+__all__ = [
+    "boxplot",
+    "correlation_network",
+    "barplot",
+    "radarplot",
+    "heatmap",
+    "streamgraph",
+    "stacked_barplot",
+]
